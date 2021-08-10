c6184068de310f6cf1d2e3ff7d0ba9c57fe44c0f
SigureRuri <50079507+SigureRuri@users.noreply.github.com>
2021-08-10 10:59:53 +0900

    QuestIDのequalsとhashcodeを追加

--- src/main/java/com/github/shur/mariaquest/quest/QuestId.kt
+++ src/main/java/com/github/shur/mariaquest/quest/QuestId.kt
@@ -9,6 +9,22 @@ class QuestId private constructor(private val id: String) {
 
     override fun toString(): String = id
 
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as QuestId
+
+        if (id != other.id) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return id.hashCode()
+    }
+
+
     companion object {
 
         private val PATTERN = "[a-z_0-9]+".toRegex()
